a8a2c48e356614ceed8d256ebed887779f6a62da
Suraj Bahadur <surajbahadur58@gmail.com>
2021-01-09 18:20:02 +0530

    asd

--- functions/create-manage-link.js
+++ functions/create-manage-link.js
@@ -61,6 +61,6 @@ exports.handler  = async (event, context) => {
 
     return {
         statusCode : 200,
-        body : JSON.stringify(result),
+        body : JSON.stringify(response),
     }
 }
