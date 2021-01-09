58daca70aebe6f191f75960621007c1fb20fd8cf
Suraj Bahadur <surajbahadur58@gmail.com>
2021-01-09 18:15:45 +0530

    wip

--- functions/create-manage-link.js
+++ functions/create-manage-link.js
@@ -1,34 +1,59 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const {faunaFetch} = require('../utils/fauna')
+// const {faunaFetch} = require('../utils/fauna')
 
 
 exports.handler  = async (event, context) => {
     const {user} = context.clientContext;
 
     console.log(user);
+    const netlifyID = user.sub;
+    // const variables = { netlifyID : user.sub}; 
+    const response = await fetch("https://graphql.fauna.com/graphql", {
+        method : 'POST',
+        headers: {
+            Authorization: `Bearer ${process.env.FAUNA_SERVER_KEY}`,
+        },
+        body : JSON.stringify({
+            query: `
+            query(netlifyID : ID!) {
+                getUserByNetlifyID(netlifyID: $netlifyID ){
+                  stripeID
+                  netlifyID
+                }
+                
+              }
+            `,  
+            variables : {
+                netlifyID,
+                
+            }
+        })
+    });
 
-    const query = `
-    query(netlifyID : ID!) {
-        getUserByNetlifyID(netlifyID: $netlifyID ){
-          stripeID
-          netlifyID
-        }
+    console.log(response);
+
+    // const query = `
+    // query(netlifyID : ID!) {
+    //     getUserByNetlifyID(netlifyID: $netlifyID ){
+    //       stripeID
+    //       netlifyID
+    //     }
         
-      }`;
+    //   }`;
 
-      const variables = { netlifyID : user.sub}; 
+    //   const variables = { netlifyID : user.sub}; 
 
-      console.log(variables);
-
-
-      const result = await faunaFetch({query, variables});
-      console.log(result);
+    //   console.log(variables);
 
 
-      const stripeID = result.data.getUserByNetlifyID.stripeID;
+    //   const result = await faunaFetch(query, variables);
+    //   console.log(result);
 
 
-      console.log(stripeID);
+    //   const stripeID = result.data.getUserByNetlifyID.stripeID;
+
+
+    //   console.log(stripeID);
 
     return {
         statusCode : 200,
