d45d1b893442b762827822ebddd8cfcfad26a1f5
Suraj Bahadur <surajbahadur58@gmail.com>
2021-01-09 17:41:22 +0530

    stripe  and netlify data retrive

---
+++ functions/create-manage-link.js
@@ -0,0 +1,31 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const {faunaFetch} = require('../utils/fauna')
+
+
+exports.handler  = async (event, context) => {
+    const {user} = context.clientContext;
+
+    console.log(user);
+
+    const query = `
+    query(netlifyID : ID!) {
+        getUserByNetlifyID(netlifyID: $netlifyID ){
+          stripeID
+          netlifyID
+        }
+        
+      }`;
+
+      const variables = { netlifyID : user.sub}; 
+
+      const result = await faunaFetch({query, variables});
+
+      const stripeID = result.data.getUserbyNetlifyId.stripeID;
+
+      console.log(stripeID);
+
+    return {
+        statusCode : 200,
+        body : JSON.stringify(result),
+    }
+}
---
+++ utils/fauna.js
@@ -0,0 +1,15 @@
+const fetch = require('node-fetch');
+
+exports.faunaFetch = async ({query , variables}) => {
+
+    return  await fetch("https://graphql.fauna.com/graphql", {
+        method : 'POST',
+        headers: {
+            Authorization: `Bearer ${process.env.FAUNA_SERVER_KEY}`,
+        },
+        body : JSON.stringify({
+            query,
+            variables,
+        })
+    });
+} 
