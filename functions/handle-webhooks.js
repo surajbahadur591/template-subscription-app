a370c12ab95b62294c3a4d67522fce4571149838
Suraj Bahadur <surajbahadur58@gmail.com>
2021-01-11 23:15:28 +0530

    stripe cli added

---
+++ functions/handle-webhooks.js
@@ -0,0 +1,28 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+exports.handler = async ({ body, headers }) => {
+    try {
+      const stripeEvent = stripe.webhooks.constructEvent(
+        body,
+        headers['stripe-signature'],
+        process.env.STRIPE_WEBHOOK_SECRET
+      );
+  
+      if (stripeEvent.type === 'customer.subscription.updated') {
+        const eventObject = stripeEvent.data.object;
+        console.log(JSON.stringify(subscription, null, 2))
+      }
+  
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ received: true }),
+      };
+    } catch (err) {
+      console.log(`Stripe webhook failed with ${err}`);
+  
+      return {
+        statusCode: 400,
+        body: `Webhook Error: ${err.message}`,
+      };
+    }
+  };
